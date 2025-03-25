e5063609971eb7b1f72cf0c59530ed563b90353e
jinohyoum <jinohyoum@gmail.com>
2025-03-25 15:51:04 +1100

    Staff Class

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Staff.java
@@ -0,0 +1,17 @@
+package com.unimelb.swen30006.workshops;
+import java.util.Date;
+
+
+public class Staff extends Person {
+    String[] roles;
+    String staffID;
+
+    public Staff(String address, Date dob, String name, String[] roles, String staffID) {
+        super(address, dob, name);
+        this.roles = roles;
+        this.staffID = staffID;
+    }
+
+    public void teaches(Subject subject) {
+    }
+}
